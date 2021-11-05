f76644be547373a90a5b81504ddc301f5a6cfe51
lilymartucci <lilyclairemartucci@gmail.com>
2021-11-05 13:52:36 -0400

    adding genderBonus method

--- WageGapData.java
+++ WageGapData.java
@@ -5,7 +5,9 @@ import java.util.*;
 public class WageGapData{
 
   public static void main (String[] args) throws FileNotFoundException{
-    System.out.println(organizeData("GenderPayGapData2122.csv"));
+    //System.out.println(organizeData("Test.csv"));
+    genderBonus("GenderPayGapData2122Mod.csv");
+
   }
 
   //which gender has the greater bonus?
@@ -17,26 +19,56 @@ public class WageGapData{
     ArrayList<ArrayList<String>> allDataList = new ArrayList<ArrayList<String>>();
     //ArrayList for each line
     ArrayList<String> currLine;
-    String[] temp = new String[23];
+    //made array variable
+    String[] temp = new String[4];
+    sc.nextLine();
     while(sc.hasNextLine()){
-      String company = sc.next();
+      String company = sc.nextLine();
+
       //then use split on that line
-      temp = company.split(",");
+      temp = company.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
       //then add line to arraylist
       currLine = new ArrayList<>(Arrays.asList(temp));
       allDataList.add(currLine);
+
     }
     return allDataList;
   }
 
-  //public static String topMedian (){
 
-  //}
   //method to look through bonus by gender and see which gender has greater bonus
-  //public static String genderBonus (){
+  public static void genderBonus (String pathname) throws FileNotFoundException{
+    ArrayList<ArrayList<String>> dataList = organizeData(pathname);
     //avg bonus percent for men
-    //avg bonus percent for women
+    //loop to add bonus percents
+    double maleBonus =0.0;
+    double femaleBonus =0.0;
+    int count =0;
 
+    for(int i=0; i<dataList.size(); i++){
+      maleBonus += Double.parseDouble(dataList.get(i).get(0));
+      count++;
+    }
+
+    for(int i=0; i<dataList.size(); i++){
+      femaleBonus += Double.parseDouble(dataList.get(i).get(1));
+    }
+
+    maleBonus = maleBonus/count;
+    femaleBonus =femaleBonus/count;
+    if(femaleBonus >  maleBonus ){
+      System.out.println("In the year of 2021-2022 female average bonus (" + femaleBonus + "%) is greater than male average bonus");
+    }
+    else if(maleBonus > femaleBonus ){
+      System.out.println("In the year of 2021-2022 male average bonus (" + maleBonus + "%) is greater than female average bonus");
+    }
+    else{
+      System.out.println("In the year of 2021-2022 female bonus and male bonus percentages are equal");
+    }
   }
+
+  //public static String topMedian (){
+    
+  //}
 
 }
